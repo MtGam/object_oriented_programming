caf3bf768c0697d80c62f5e9da43630251ee575e
MtGam <theresm@gmail.com>
2016-06-02 14:24:01 -0400

    instances of cat tried in irb

--- cats.rb
+++ cats.rb
@@ -2,8 +2,27 @@
 #2 gave cat attributes
 #3 added initialize method
 class Cat
-  def initialize(name, preferred_food, meal_time)
-    @name = name
+  def initialize(cat_name, preferred_food, meal_time)
+    @cat_name = cat_name
     @preferred_food = preferred_food
     @meal_time = meal_time
+
+  end
+
+  def cat_name
+    @cat_name
+  end
+
+  def preferred_food
+    @preferred_food
+  end
+
+  def meal_time
+    @meal_time
+  end
+
+  def eats_at
+    
+
+
 end
