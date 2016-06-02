22292de68fdeeeba95712cf5c0b993fb32d54e53
MtGam <theresm@gmail.com>
2016-06-02 13:19:12 -0400

    cat with initialize method

---
+++ cats.rb
@@ -0,0 +1,9 @@
+#1 added class 'Cat'
+#2 gave cat attributes
+#3 added initialize method
+class Cat
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+end
