82aee6938d3da52f21d5b5b35035205dd54234f0
MtGam <theresm@gmail.com>
2016-06-02 13:11:31 -0400

    cat's meow started

---
+++ people.rb
@@ -0,0 +1,15 @@
+# 1 created Person and Student class
+# 2 methods learn and teach
+
+class Person
+
+
+class Student
+  def learn
+    "I get it!"
+end
+
+class Instructor
+  def teach
+    "Everything in Ruby is an Object."
+end
