a8b42710d8e37939cdde20fbf719140e0464d30c
MtGam <theresm@gmail.com>
2016-06-02 15:33:00 -0400

    cat's eating time in am/pm

--- cats.rb
+++ cats.rb
@@ -2,11 +2,11 @@
 #2 gave cat attributes
 #3 added initialize method
 class Cat
+
   def initialize(cat_name, preferred_food, meal_time)
     @cat_name = cat_name
     @preferred_food = preferred_food
     @meal_time = meal_time
-
   end
 
   def cat_name
@@ -21,8 +21,22 @@ class Cat
     @meal_time
   end
 
+#4
   def eats_at
-    
+    @meal_time
+     if @meal_time > 12
+      "#{@meal_time - 12}pm"
+     else
+      "#{@meal_time}am"
+     end
+  end
+
+
+#5 Cat introduction with string method
+  def meow
+    "My name is #{@cat_name} and I eat at #{eats_at}"
+  end
+
 
 
 end
