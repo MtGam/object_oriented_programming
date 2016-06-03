4d7af1d45ae914a4094a57dc0f28b70770245ba7
MtGam <theresm@gmail.com>
2016-06-02 21:45:58 -0400

    added paperboy report

--- paperboy.rb
+++ paperboy.rb
@@ -1,21 +1,35 @@
 # created class
 class Paperboy
 
+  attr_accessor :name, :experience, :side
 
+  attr_reader :earnings
 
-  def initialize(name,quota,experience,side,earnings)
+  def initialize(name, side)
     @name = name
-    @quota = 50
-    @experience = experience
+    @experience = 0
     @side = side
-    @earnings = earnings
+    @earnings = 0
   end
 
 
   def quota
-    (experience/2) + 50
+    (@experience/2) + 50 #minimum number of papers to deliver=50
+
   end
 
 
+  def deliver(start_address, end_address)
+    num_houses = end_address - start_address
+    # with the assumption that start adddress as less than end address
+    @experience += num_houses
+    pay = num_houses.to_f * 0.25
+    @earnings = @earnings + pay
+    return pay
+  end
+
+  def report
+    "I'm #{@name}, I've delivered #{@experience} papers and I've earned #{@earnings}."
+  end
 
 end
