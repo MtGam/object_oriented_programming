c3693c5295b78a1ab9546af174e0ae76b30a1e32
MtGam <theresm@gmail.com>
2016-06-03 13:18:53 -0400

    mars_rover exercise added

--- paperboy.rb
+++ paperboy.rb
@@ -21,12 +21,14 @@ class Paperboy
 
   def deliver(start_address, end_address)
     num_houses = end_address - start_address
-    # with the assumption that start adddress as less than end address
+    # with the assumption that start adddress is less than end address
     @experience += num_houses
     pay = num_houses.to_f * 0.25
     @earnings = @earnings + pay
     return pay
   end
+
+
 
   def report
     "I'm #{@name}, I've delivered #{@experience} papers and I've earned #{@earnings}."
