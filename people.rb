8802115f490b1c0951b55c10e30f68cbd06c4abd
MtGam <theresm@gmail.com>
2016-06-05 15:29:08 -0400

    class time exercise wrapped up

--- people.rb
+++ people.rb
@@ -1,13 +1,20 @@
+# Person as parent class
 class Person
+
+attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
+  def greeting
+    puts "Hi, my name is #{@name}."
+  end
+
 end
 
 
-class Student
+class Student < Person
 
   def learn
     puts "I get it!"
@@ -17,10 +24,20 @@ end
 
 
 
-class Instructor
+class Instructor < Person
 
   def teach
     puts "Everything in Ruby is an Object!"
   end
 
 end
+
+# instance of instructor named Chris and the call to his greeting/teach
+chris = Instructor.new("Chris")
+chris.greeting
+chris.teach
+
+#instance of student named Cristina and the call to her greeting/learn
+cristina = Student.new("Cristina")
+cristina.greeting
+cristina.learn
